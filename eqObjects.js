8ef74a7d80b35356f70c41d3fac33a2cb213265c
David Carmichael <carmichael.db88@gmail.com>
2023-09-28 04:59:36 +0000

    linted eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -7,8 +7,8 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//create function that takes two arrays and returns true or false based on if arrays are perfect match
-const eqArrays = function (arr1, arr2) {
+//function that takes two arrays and returns true or false based on if arrays are perfect match
+const eqArrays = function(arr1, arr2) {
   let result;
   for (let x = 0; x < arr1.length; x++) {
     if (arr1[x] === arr2[x] && arr1.length === arr2.length) {
